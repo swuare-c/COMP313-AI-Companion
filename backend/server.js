e3fbb3c77fe28c704e1a0f36a5a6a42b85a144ce
Christian Khan <christiankhan100@gmail.com>
2026-02-12 20:55:30 -0500

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "API is running." })
+})
 
 app.listen(PORT, () => {
-    console.log("API is running.")
+    console.log(`API is running on port: ${PORT}.`)
 })
